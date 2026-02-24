f168bc132cbe51b9fe57655bc36746ad9ce0bd10
FredVenturin <frederico.venturin@gmail.com>
2026-02-24 00:06:19 -0300

    feat: ending project and making routes

--- src/use_cases/registry_finder.py
+++ src/use_cases/registry_finder.py
@@ -9,11 +9,11 @@ class RegistryFinder:
     def __init__(self, orders_repository: OrdersRepositoryInterface):
         self.__orders_repository = orders_repository
 
-    def find(self, http_request: HttpRequest) ->HttpResponse:
+    def find(self, http_request: HttpRequest) -> HttpResponse:
         try:
             order_id = http_request.path_params["order_id"]
             order = self.__search_order(order_id)
-            self.__format_response(order)
+            return self.__format_response(order)    
         except Exception as exception:
             return error_handler(exception)
 
@@ -22,10 +22,12 @@ class RegistryFinder:
         if not order: raise HttpNotFoundError("Order not found")
         return order
     
-    def __format_response(self, order: dict) ->HttpResponse:
+    def __format_response(self, order: dict) -> HttpResponse:
+        order["_id"] = str(order["_id"])    
+
         return HttpResponse(
             body={
-                "data":{
+                "data": {
                     "count": 1,
                     "type": "Order",
                     "attributes": order
