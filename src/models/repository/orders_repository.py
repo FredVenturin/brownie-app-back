dd717c7f8130e6c3ec1f753bfdefc10f343c2ba2
FredVenturin <frederico.venturin@gmail.com>
2026-02-26 16:33:46 -0300

    config adjusting orders repo

--- src/models/repository/orders_repository.py
+++ src/models/repository/orders_repository.py
@@ -1,80 +1,97 @@
 from bson.objectid import ObjectId
 from src.models.repository.interfaces.orders_repository_interface import OrdersRepositoryInterface
 
+
 class OrdersRepository(OrdersRepositoryInterface):
-    def __init__(self, db_connection) ->None:
+
+    def __init__(self, db_connection) -> None:
         self.__collection_name = "orders"
         self.__db_connection = db_connection
 
-    def insert_document(self, document:dict) ->None:
+
+    def insert_document(self, document: dict) -> None:
         collection = self.__db_connection.get_collection(self.__collection_name)
         collection.insert_one(document)
 
-    def insert_list_of_documents(self, list_of_documents: list) ->None:
+
+    def insert_list_of_documents(self, list_of_documents: list) -> None:
         collection = self.__db_connection.get_collection(self.__collection_name)
         collection.insert_many(list_of_documents)
 
-    def select_many(self, doc_filter: dict) ->list:
+
+    def select_many(self, doc_filter: dict) -> list:
         collection = self.__db_connection.get_collection(self.__collection_name)
-        data = collection.find(doc_filter)
-        return data
-    
-    def select_one(self, doc_filter:dict) ->dict:
+        return collection.find(doc_filter)
+
+
+    def select_one(self, doc_filter: dict) -> dict:
         collection = self.__db_connection.get_collection(self.__collection_name)
-        response = collection.find_one(doc_filter)
-        return response
-    
-    def select_many_with_properties(self, doc_filter: dict) ->list:
+        return collection.find_one(doc_filter)
+
+
+    def select_many_with_properties(self, doc_filter: dict, projection: dict) -> list:
         collection = self.__db_connection.get_collection(self.__collection_name)
-        data = collection.find(
-            doc_filter,
-            { "_id": 0, "cupom": 0}
+        return collection.find(doc_filter, projection)
+
+
+    def select_if_property_exists(self, property_name: str) -> dict:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        return collection.find_one(
+            {property_name: {"$exists": True}}
         )
-        return data
-    
-    def select_if_property_exists(self) ->dict:
+
+
+    def select_by_object_id(self, object_id: str) -> dict:
         collection = self.__db_connection.get_collection(self.__collection_name)
-        response = collection.find_one(
-            {"address": {"$exists": True}},
-            { "_id": 0, "cupom": 0}
-        )
-        return response
-    
-    def select_by_object_id(self, object_id: str) ->dict:
+        return collection.find_one({"_id": ObjectId(object_id)})
+
+
+    def edit_registry(self, order_id: str, update_fields: dict) -> bool:
         collection = self.__db_connection.get_collection(self.__collection_name)
-        data = collection.find_one({"_id": ObjectId(object_id)})
-        return data
-    
-    def edit_registry(self, order_id:str, update_fields: dict) ->None:
-        collection = self.__db_connection.get_collection(self.__collection_name)
-        collection.update_one(
+
+        result = collection.update_one(
             {"_id": ObjectId(order_id)},
             {"$set": update_fields}
         )
 
-    def edit_many_registries(self) ->None:
+        return result.modified_count > 0
+
+
+    def edit_many_registries(self, doc_filter: dict, update_fields: dict) -> int:
         collection = self.__db_connection.get_collection(self.__collection_name)
-        collection.update_many(
-            {"itens.refrigerante": {"$exists": True}},
-            {"$set": {"itens.pizza.quantidade": 30}}
+
+        result = collection.update_many(
+            doc_filter,
+            {"$set": update_fields}
         )
 
-    def edit_registry_with_increment(self) ->None:
+        return result.modified_count
+
+
+    def edit_registry_with_increment(self, doc_filter: dict, increment_fields: dict) -> int:
         collection = self.__db_connection.get_collection(self.__collection_name)
-        collection.update_many(
-            {"itens.refrigerante": {"$exists": True}},
-            {"$inc": {"itens.pizza.quantidade": 50}}
+
+        result = collection.update_many(
+            doc_filter,
+            {"$inc": increment_fields}
         )
+
+        return result.modified_count
+
 
     def delete_registry(self, order_id: str) -> bool:
         collection = self.__db_connection.get_collection(self.__collection_name)
+
         result = collection.delete_one(
             {"_id": ObjectId(order_id)}
         )
 
         return result.deleted_count > 0
-    
-    def delete_many_registries(self) ->None:
-        collection = collection = self.__db_connection.get_collection(self.__collection_name)
-        collection.delete_many({"itens.refrigerante": {"$exists": True}})
 
+
+    def delete_many_registries(self, doc_filter: dict) -> int:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+
+        result = collection.delete_many(doc_filter)
+
+        return result.deleted_count
